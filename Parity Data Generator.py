ac60694aa523730890c4220448dcc6a16e3a2c0c
Kevin <ktrebing@uni-osnabrueck.de>
2017-01-06 10:45:37 +0100

    Parity Data Generator done

---
+++ Parity Data Generator.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def generateParityData(N=1000, n=3, randomstate=42):
+
+    rng = np.random.RandomState(randomstate)
+    bits = rng.randint(2, size=N)
+
+    # Calculate n-parity
+    parity = n*[0]
+    for i in range(n, N):
+            odd = False
+            for j in range(i-n,i):
+                if bits[j] == 1:
+                    odd = not odd
+            parity.append(1 if odd else 0)
+    return bits, np.array(parity)
+
+
+bits, parity = generateParityData()
+
+# print("len(bits):",len(bits), "len(parity)",len(parity))
+# print(bits[0:20])
+# print(parity[0:20])
