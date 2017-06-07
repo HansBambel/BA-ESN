27647da7d1c35bbe7d31690b84efae964aa6554e
Kevin <ktrebing@uni-osnabrueck.de>
2017-06-07 16:07:28 +0200

    started data plotting

--- dataPlotting.py
+++ dataPlotting.py
@@ -1,23 +1,57 @@
 import pandas as pd
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 with open("squashing_scripterrors/all_squashing_errors_dataframe.p", "rb")as inputfile:
-    df = pickle.load(inputfile)
+    df = pd.DataFrame(pickle.load(inputfile))
 
-print(np.min(df["averaged_error"]))
-# print(df)
+# print(np.min(df["averaged_error"]))
+min_row = df.loc[df["averaged_error"]==np.min(df["averaged_error"])]
+print(min_row.iloc[0]["input_leak_rate"])
+print(min_row.iloc[0]["input_spectral_rad"])
 
-with open("randomprojection_scripterrors/all_randomprojection_errors_dataframe.p", "rb")as inputfile:
-    df = pickle.load(inputfile)
 
-print(np.min(df["averaged_error"]))
-# print(df)
+# get inputESN parameter for lowest error
+grouped = df.loc[(df["input_leak_rate"]==min_row.iloc[0]["input_leak_rate"]) &
+                 (df["input_spectral_rad"]==min_row.iloc[0]["input_spectral_rad"])]
 
-with open("squashing_scripterrors/esnError00001.p", "rb") as inputfile:
-    temp = pickle.load(inputfile)
-print(temp)
+### TODO get rid of outlier
+print(grouped.loc[grouped["averaged_error"]==np.max(grouped["averaged_error"])].iloc[0]["errors"])
 
-with open("randomprojection_scripterrors/esnError00001.p", "rb") as inputfile:
-    temp = pickle.load(inputfile)
-print(temp)
+grouped = grouped.pivot(index="output_leak_rate", columns="output_spectral_rad",values="averaged_error")
+X=grouped.index.values
+Y=grouped.columns.values
+Z=grouped.values
+xx,yy = np.meshgrid(X,Y)
+plt.contourf(xx,yy,Z.T,30)
+plt.colorbar()
+plt.show()
+
+############# Randomprojection
+# with open("randomprojection_scripterrors/all_randomprojection_errors_dataframe.p", "rb")as inputfile:
+#     df = pd.DataFrame(pickle.load(inputfile))
+#
+# # print(np.min(df["averaged_error"]))
+# min_row = df.loc[df["averaged_error"]==np.min(df["averaged_error"])]
+# print(min_row.iloc[0]["input_leak_rate"])
+# print(min_row.iloc[0]["input_spectral_rad"])
+#
+#
+# # get inputESN parameter for lowest error
+# grouped = df.loc[(df["input_leak_rate"]==min_row.iloc[0]["input_leak_rate"]) &
+#                  (df["input_spectral_rad"]==min_row.iloc[0]["input_spectral_rad"])]
+#
+# ### TODO get rid of outlier
+# print(grouped.loc[grouped["averaged_error"]==np.max(grouped["averaged_error"])].iloc[0]["errors"])
+#
+# grouped = grouped.pivot(index="output_leak_rate", columns="output_spectral_rad",values="averaged_error")
+# X=grouped.index.values
+# Y=grouped.columns.values
+# Z=grouped.values
+# xx,yy = np.meshgrid(X,Y)
+# plt.contourf(xx,yy,Z.T,30)
+# plt.colorbar()
+# plt.show()
+
--- dataSummarizer.py
+++ dataSummarizer.py
@@ -1,34 +1,21 @@
 import pickle
 import pandas as pd
 
+### Save squashing errors in array to a pickle file ###
 # all = []
 # for i in range(0,10000):
 #     with open("squashing_scripterrors/esnError{:>05}.p".format(i), "rb") as inputfile:
 #         all.append(pickle.load(inputfile))
-#
-# print(len(all))
-# # print(range(0,10000))
 # with open("squashing_scripterrors/all_squashing_errors.p", "wb") as outputfile:
 #     pickle.dump(all, outputfile)
 
-# with open("randomprojection_scripterrors/all_randomprojection_errors.p", "rb") as file:
-#     all_errors = pickle.load(file)
-
-# with open("squashing_scripterrors/all_squashing_errors.p", "rb") as file:
-#     all_errors = pickle.load(file)
-
-# with open("squashing_scripterrors/esnError00001.p", "rb") as inputfile:
-#     temp = pickle.load(inputfile)
-# print(temp)
-# print(temp["params"]["input_leak_rate"])
-
-# df = pd.DataFrame([temp["params"]["input_leak_rate"], temp["params"]["input_spectral_rad"],
-#                    temp["params"]["output_leak_rate"], temp["params"]["output_spectral_rad"],
-#                    temp["errors"], temp["averaged_error"]])
-
-# df = pd.DataFrame({"input_leak_rate":temp["params"]["input_leak_rate"], "input_spectral_rad":temp["params"]["input_spectral_rad"],
-#                    "output_leak_rate":temp["params"]["output_leak_rate"], "output_spectral_rad":temp["params"]["output_spectral_rad"],
-#                    "averaged_error":temp["averaged_error"]})
+### Save randomprojection errors in array to a pickle file ###
+# all = []
+# for i in range(0,10000):
+#     with open("randomprojection_scripterrors/esnError{:>05}.p".format(i), "rb") as inputfile:
+#         all.append(pickle.load(inputfile))
+# with open("randomprojection_scripterrors/all_randomprojection_errors.p", "wb") as outputfile:
+#     pickle.dump(all, outputfile)
 
 df = pd.DataFrame(columns=["input_leak_rate","input_spectral_rad",
                             "output_leak_rate","output_spectral_rad",
