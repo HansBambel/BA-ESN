c17bd68319e9afe2af9301c0c8d72567bf0d070b
Kevin <ktrebing@uni-osnabrueck.de>
2017-06-08 11:31:41 +0200

    cleaned dataPlotting

--- dataPlotting.py
+++ dataPlotting.py
@@ -9,8 +9,7 @@ with open("squashing_scripterrors/all_squashing_errors_dataframe.p", "rb")as inp
 
 # print(np.min(df["averaged_error"]))
 min_row = df.loc[df["averaged_error"]==np.min(df["averaged_error"])]
-print(min_row.iloc[0]["input_leak_rate"])
-print(min_row.iloc[0]["input_spectral_rad"])
+print(min_row[["input_leak_rate", "input_spectral_rad"]])
 
 
 # get inputESN parameter for lowest error
@@ -18,7 +17,8 @@ grouped = df.loc[(df["input_leak_rate"]==min_row.iloc[0]["input_leak_rate"]) &
                  (df["input_spectral_rad"]==min_row.iloc[0]["input_spectral_rad"])]
 
 ### TODO get rid of outlier
-print(grouped.loc[grouped["averaged_error"]==np.max(grouped["averaged_error"])].iloc[0]["errors"])
+print("Errors of max: ",grouped.loc[grouped["averaged_error"]==np.max(grouped["averaged_error"])].iloc[0]["errors"])
+print("Errors of min",grouped.loc[grouped["averaged_error"]==np.min(grouped["averaged_error"])].iloc[0]["errors"])
 
 grouped = grouped.pivot(index="output_leak_rate", columns="output_spectral_rad",values="averaged_error")
 X=grouped.index.values
@@ -30,28 +30,28 @@ plt.colorbar()
 plt.show()
 
 ############# Randomprojection
-# with open("randomprojection_scripterrors/all_randomprojection_errors_dataframe.p", "rb")as inputfile:
-#     df = pd.DataFrame(pickle.load(inputfile))
-#
-# # print(np.min(df["averaged_error"]))
-# min_row = df.loc[df["averaged_error"]==np.min(df["averaged_error"])]
-# print(min_row.iloc[0]["input_leak_rate"])
-# print(min_row.iloc[0]["input_spectral_rad"])
-#
-#
-# # get inputESN parameter for lowest error
-# grouped = df.loc[(df["input_leak_rate"]==min_row.iloc[0]["input_leak_rate"]) &
-#                  (df["input_spectral_rad"]==min_row.iloc[0]["input_spectral_rad"])]
-#
-# ### TODO get rid of outlier
-# print(grouped.loc[grouped["averaged_error"]==np.max(grouped["averaged_error"])].iloc[0]["errors"])
-#
-# grouped = grouped.pivot(index="output_leak_rate", columns="output_spectral_rad",values="averaged_error")
-# X=grouped.index.values
-# Y=grouped.columns.values
-# Z=grouped.values
-# xx,yy = np.meshgrid(X,Y)
-# plt.contourf(xx,yy,Z.T,30)
-# plt.colorbar()
-# plt.show()
+with open("randomprojection_scripterrors/all_randomprojection_errors_dataframe.p", "rb")as inputfile:
+    df = pd.DataFrame(pickle.load(inputfile))
+
+# print(np.min(df["averaged_error"]))
+min_row = df.loc[df["averaged_error"]==np.min(df["averaged_error"])]
+print(min_row[["input_leak_rate", "input_spectral_rad"]])
+
+
+# get inputESN parameter for lowest error
+grouped = df.loc[(df["input_leak_rate"]==min_row.iloc[0]["input_leak_rate"]) &
+                 (df["input_spectral_rad"]==min_row.iloc[0]["input_spectral_rad"])]
+
+### TODO get rid of outlier
+print("Errors of max: ",grouped.loc[grouped["averaged_error"]==np.max(grouped["averaged_error"])].iloc[0]["errors"])
+print("Errors of min",grouped.loc[grouped["averaged_error"]==np.min(grouped["averaged_error"])].iloc[0]["errors"])
+
+grouped = grouped.pivot(index="output_leak_rate", columns="output_spectral_rad",values="averaged_error")
+X=grouped.index.values
+Y=grouped.columns.values
+Z=grouped.values
+xx,yy = np.meshgrid(X,Y)
+plt.contourf(xx,yy,Z.T,30)
+plt.colorbar()
+plt.show()
 
