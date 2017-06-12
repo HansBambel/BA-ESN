9f67bfa346c637da68676dd1be3ea1ff3beb72dc
Kevin <ktrebing@uni-osnabrueck.de>
2017-06-12 12:19:48 +0200

    marginalized errors and plotted errors

---
+++ dataPlottingArray.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+with open("randomprojection_scripterrors/all_randomprojection_errors.p", "rb") as inputfile:
+    data = pickle.load(inputfile)
+
